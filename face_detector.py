767da30667cf55992780bb86274edd94f1277c4d
Nalugya31 <vanessanalugya@gmail.com>
2023-11-27 05:19:26 +0300

    Real time face detector app completed

--- face_detector.py
+++ face_detector.py
@@ -3,10 +3,39 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # img = cv2.imread('robert.png')
-img = cv2.imread('download.jpeg')
+# img = cv2.imread('download.jpeg')
+#To capture video from the webcam
+webcam = cv2.VideoCapture(0)
 
-grayscaled_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+while True:
 
+    successful_frame_read,frame = webcam.read()
+
+#Must convert to grayscale.
+    grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    #draw rectangles around faces
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(randrange(256),randrange(256),randrange(256)),2)
+
+
+    cv2.imshow('vanessa face detector',frame)
+
+
+    key = cv2.waitKey(1)
+#stop if Q key is pressed
+    if key==81 or key==113:
+        break
+
+# Release the videocapture object
+webcam.release()
+
+
+
+
+
+'''
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 #draw rectangles around faces
 for (x,y,w,h) in face_coordinates:
@@ -24,7 +53,7 @@ cv2.imshow('vanessa face detector',img)
 
 cv2.waitKey()
 
- 
+ '''
 
 
 
